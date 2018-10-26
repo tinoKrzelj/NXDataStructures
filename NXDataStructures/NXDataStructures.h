2e2b1d7c57ee71318234d98ee00501d1f239fab6
tino.krzelj <tino.krzelj@icloud.com>
2018-10-26 14:44:35 +0200

    Initial Commit

---
+++ NXDataStructures/NXDataStructures.h
@@ -0,0 +1,19 @@
+//
+//  NXDataStructures.h
+//  NXDataStructures
+//
+//  Created by Tino Krželj on 26/10/2018.
+//  Copyright © 2018 Tino Krželj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NXDataStructures.
+FOUNDATION_EXPORT double NXDataStructuresVersionNumber;
+
+//! Project version string for NXDataStructures.
+FOUNDATION_EXPORT const unsigned char NXDataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NXDataStructures/PublicHeader.h>
+
+
