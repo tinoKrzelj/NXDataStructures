67e9d18060409be4b9c481cc56d9643e445aab9e
Tino Krzelj <tino.krzelj@icloud.com>
2019-02-20 19:14:42 +0100

    Update Project folders, Add NXQueue placeholder

---
+++ NXDataStructures/Data Structures/Queue/NXQueue.swift
@@ -0,0 +1,9 @@
+//
+//  NXQueue.swift
+//  NXDataStructures
+//
+//  Created by Tino Krželj on 20/02/2019.
+//  Copyright © 2019 Tino Krželj. All rights reserved.
+//
+
+import Foundation
---
+++ NXDataStructuresTests/DataStructures/Queue/NXQueue+Test.swift
@@ -0,0 +1,24 @@
+//
+//  NXQueue+Test.swift
+//  NXDataStructuresTests
+//
+//  Created by Tino Krželj on 20/02/2019.
+//  Copyright © 2019 Tino Krželj. All rights reserved.
+//
+
+import XCTest
+@testable import NXDataStructures
+
+class NXQueue_Test: XCTestCase {
+    //
+    // MARK: Properties
+    //
+    
+    override func setUp() {}
+    override func tearDown() {}
+    
+    //
+    // MARK: Custom Methods
+    //
+
+}
